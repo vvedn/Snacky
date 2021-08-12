de7f1d25130ad7801b52d2f2ff5ea69983066ded
Vaishnavi V <vved42@gmail.com>
2021-08-12 11:41:34 -0400

    just testing

--- SnackyApp/ContentView.swift
+++ SnackyApp/ContentView.swift
@@ -7,10 +7,22 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+        ZStack {
+                    Color.green
+                        .ignoresSafeArea()
+            Text("Welcome to Snacky.")
+                .font(.system(size: 50,design: .serif))
+                .italic()
+            
+                    // Your other content here
+                    // Other layers will respect the safe area edges
+            }
+        
     }
 }
 

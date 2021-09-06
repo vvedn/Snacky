ca16662590a385bdaa83a74bfa32d0c094c2da88
Tobechi <tobechionwuka@gmail.com>
2021-09-06 11:59:36 -0400

    changed craving view

--- SnackyApp/CravingView.swift
+++ SnackyApp/CravingView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct CravingView: View {
     var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.green, .blue]), startPoint: .top, endPoint: .bottom)
+                        .ignoresSafeArea()
+ 
         VStack{
             //start of buttons
+            
             Text("What are you feeling right now?")
         Button("Salty", action: {
             print("Craving")
@@ -21,6 +26,8 @@ struct CravingView: View {
         })
         }
     }
+}
+         
 }
 
 struct SwiftUIView_Previews: PreviewProvider {

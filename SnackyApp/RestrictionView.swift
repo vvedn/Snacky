a3be61e91c43ae30558c1060126ed999ba3e6946
Vaishnavi V <vved42@gmail.com>
2021-09-12 18:52:05 -0400

    new restrictions file

---
+++ SnackyApp/RestrictionView.swift
@@ -0,0 +1,36 @@
+//
+//  RestrictionView.swift
+//  SnackyApp
+//
+//  Created by V Ved on 9/12/21.
+//
+
+import SwiftUI
+
+struct RestrictionView: View {
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.green, .blue]), startPoint: .top, endPoint: .bottom)
+                        .ignoresSafeArea()
+ 
+        VStack{
+            //start of buttons
+            
+            Text("Do you have any dietary restrictions?")
+        Button("Yes", action: {
+            print("Dietary Restriction")
+        })
+            
+        Button("Sweet", action: {
+            print("No Restriction")
+        })
+        }
+    }
+    }
+}
+
+struct RestrictionView_Previews: PreviewProvider {
+    static var previews: some View {
+        RestrictionView()
+    }
+}
